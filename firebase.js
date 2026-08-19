3e293fc8367b6928ea7f880981f18dbb80416a2c
unknown <bariogajohnmark@gmail.com>
2026-08-19 21:01:24 +0800

    Added README.md

--- firebase.js
+++ firebase.js
@@ -1,16 +1,21 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDKNg9FT3Dfx5OV9KmyVIOrezYkydugQgc",
-  authDomain: "cpserc-attendance-5f22e.firebaseapp.com",
-  projectId: "cpserc-attendance-5f22e",
-  storageBucket: "cpserc-attendance-5f22e.firebasestorage.app",
-  messagingSenderId: "948999206182",
-  appId: "1:948999206182:web:41a6d9b6e175295b803de7",
-  measurementId: "G-39CK6JCZEG"
+  apiKey: "AIzaSyBou2Fgw7Zj5YVFVkLebSfZ9FbazHafANw",
+  authDomain: "cpserc-attendance-a82f7.firebaseapp.com",
+  projectId: "cpserc-attendance-a82f7",
+  storageBucket: "cpserc-attendance-a82f7.firebasestorage.app",
+  messagingSenderId: "881558379605",
+  appId: "1:881558379605:web:bc236be22604e4a63788dc",
+  measurementId: "G-YZ4KYGM5DL"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
